6d189c5911e653664bce0e593f843079d52912f2
Relentless-Machine <149186957+Relentless-Machine@users.noreply.github.com>
2025-06-10 08:26:38 +0800

    feat: Add basic LoginPage and LoginForm structure

---
+++ couchat-frontend/src/components/LoginForm.tsx
@@ -0,0 +1,21 @@
+// LoginForm.tsx
+import React from 'react';
+
+const LoginForm: React.FC = () => {
+  return (
+    <form>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
+
