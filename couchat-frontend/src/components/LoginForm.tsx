ebb5972a4710e2e3b26db92dee89d125f4d06fa0
Relentless-Machine <149186957+Relentless-Machine@users.noreply.github.com>
2025-06-10 08:28:54 +0800

    feat: Implement basic state and handlers for LoginForm

--- couchat-frontend/src/components/LoginForm.tsx
+++ couchat-frontend/src/components/LoginForm.tsx
@@ -1,16 +1,52 @@
 // LoginForm.tsx
-import React from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 
 const LoginForm: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevent default form submission
+    console.log('Attempting login with:');
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Here you would typically call an authentication service
+    // For now, we just log to console and clear fields
+    setUsername('');
+    setPassword('');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" />
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={handleUsernameChange}
+          required
+        />
       </div>
       <div>
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
       </div>
       <button type="submit">Login</button>
     </form>
@@ -18,4 +54,3 @@ const LoginForm: React.FC = () => {
 };
 
 export default LoginForm;
-
